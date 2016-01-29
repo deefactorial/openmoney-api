2dd26a6b7e117eaa5f7d296316bcdf43373b5e32
deefactorial <deefactorial@gmail.com>
2016-01-29 13:24:34 -0800

    redirect user to stewards end points

--- api/oauth2server/site.js
+++ api/oauth2server/site.js
@@ -13,7 +13,23 @@ exports.loginForm = function(req, res) {
   res.render('login');
 };
 
-exports.login = passport.authenticate('local', { successReturnToOrRedirect: '/account', failureRedirect: '/login' });
+exports.login = function(req, res, next) {
+  // generate the authenticate method and pass the req/res
+  passport.authenticate('local', function(err, user, info) {
+    if (err) { return next(err); }
+    if (!user) { return res.redirect('/V2/stewards/'+ req.swagger.params.stewardname.value + '/login'); }
+
+    // req / res held in closure
+    req.logIn(user, function(err) {
+      if (err) { return next(err); }
+      //return res.send(user);
+      return res.redirect('/V2/stewards/' + req.swagger.params.stewardname.value + '/account');
+    });
+
+  })(req, res, next);
+  //passport.authenticate('local', { successReturnToOrRedirect: '/V2/stewards/' + req.swagger.params.stewardname.value + '/account', failureRedirect: '/V2/stewards/'+ req.swagger.params.stewardname.value + '/login' });
+};
+//exports.login = passport.authenticate('local', { successReturnToOrRedirect: '/V2/stewards/{stewardname}/account', failureRedirect: '/V2/stewards/{stewardname}/login' });
 
 exports.logout = function(req, res) {
   req.logout();
